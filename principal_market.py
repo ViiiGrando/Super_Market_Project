7863a4bd4c22e3d8134e87717dbafa1c1153ff22
Victor Grando <victorgrando09@gmail.com>
2025-03-04 17:05:44 -0300

    Add login and registration templates; update index routing

--- principal_market.py
+++ principal_market.py
@@ -1,18 +1,31 @@
-from flask import Flask, render_template,request,redirect
+from flask import Flask, render_template,request,redirect, session
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+db = SQLAlchemy(app)
 
 
 @app.route("/")
 def index():
- return render_template('index.html')
+   if 'user' in session:
+        return redirect('/')
+   else:
+        return render_template('login.html')
+ 
 
 
+@app.route("/login")
+def login(): 
+        return render_template('login.html')
 
+
+@app.route("/register")
+def register():
+    return redirect('register.html')
 
 @app.route("/cart")
 def cart():
-    return render_template("/cart.html")
+    return render_template('/cart.html')
 
     
 
